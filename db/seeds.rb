30104a543c8090462627f5d5e9f50895efca6db8
篠原知也 <shinoharatomoya@ShinoryMacBook-Pro.local>
2022-07-18 18:54:28 +0900

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.new(login_id: "user", :password => "password", :password_confirmation => "password")
+user.save
