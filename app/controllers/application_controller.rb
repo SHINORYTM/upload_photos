ff1063a4f3b755209a9122787e1c4f840d55024b
篠原知也 <shinoharatomoya@ShinoryMacBook-Pro.local>
2022-07-18 19:04:59 +0900

    add getting current_user and check authenticate func

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+require 'net/http'
+
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def check_authenticate
+    return if current_user
+
+    redirect_to controller: :sessions, action: :new
+  end
 end
