3192f00864c3f7798923082e3c259a13d19410a8
篠原知也 <shinoharatomoya@ShinoryMacBook-Pro.local>
2022-07-18 18:54:57 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "photos#index"
+  resources :photos, only: [:index, :new, :create]
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  get    'oauth/callback' => 'oauth#callback'
+  post    'oauth/tweet' => 'oauth#tweet'
 end
