030e0523a11ebb16227faaf88ee6b30e3978ed7c
篠原知也 <shinoharatomoya@ShinoryMacBook-Pro.local>
2022-07-18 19:04:24 +0900

    add uri create method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def auth_uri
+    uri = ENV['OAUTH_URI']
+    response_type = ENV['OAUTH_RESPONSE_TYPE']
+    client_id = ENV['OAUTH_CLIENT_ID']
+    rediredct_uri = ENV['OAUTH_REDIRECT_URI']
+
+    uri + '?response_type=' + response_type + '&redirect_uri=' + rediredct_uri + '&client_id=' + client_id
+  end
+
+  def image_uri(photo)
+    "http://localhost/3000/" + photo.filename
+  end
 end
